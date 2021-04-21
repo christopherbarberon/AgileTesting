3c07130472c714b07801034a7d7134b3e70dde39
Lena Pancher <lenapancher@gmail.com>
2021-04-21 14:37:48 +0200

    first commit

---
+++ src/codingfactory/rpgconsole/hero/Hero.java
@@ -0,0 +1,53 @@
+package codingfactory.rpgconsole.hero;
+
+import codingfactory.rpgconsole.enemy.Enemy;
+import java.util.Random;
+
+public class Hero {
+
+    private String name;
+    private int level;
+    private int hp;
+    private int atk;
+
+    public Hero(String name){ //construct
+        this.name = name;
+        this.level = 1;
+        this.hp = 20;
+        this.atk = 2;
+    }
+
+    public String getName() {
+       return this.name;
+    }
+
+    public int getHp(){
+        return this.hp;
+    }
+
+    public int getLevel(){
+        return this.level;
+    }
+
+    public int getAtk(){
+        return this.atk;
+    }
+
+    public void setLevel(int newLevel) {
+        this.level = newLevel;
+    }
+
+    public void takeDamage(int damage){
+        this.hp -= damage;
+    }
+
+    public void attack(Enemy enemy) {
+        Random rand = new Random();
+        int randomNum = rand.nextInt(this.level + 1);
+        enemy.takeDamage(this.atk+randomNum); //enemy take damage for hero attack
+    }
+
+    public void levelUp() {
+        this.setLevel(this.level+1);
+    }
+}
---
+++ src/test/HeroTest.java
@@ -0,0 +1,84 @@
+package test;
+
+import org.junit.Test;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+import codingfactory.rpgconsole.hero.Hero;
+import codingfactory.rpgconsole.enemy.Enemy;
+
+public class HeroTest {
+
+	Hero hero;
+	Enemy enemy;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		System.out.println("Avant le démarrage");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		System.out.println("Après tous les tests");
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		hero = new Hero("Jaina Portvaillant");
+		enemy = new Enemy("Lord Voldemort", 1);
+		System.out.println("Avant un test");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("Après un test");
+	}
+
+	@Test
+	public void testHeroLevelUp() throws Exception {
+		assertThat(hero, hasProperty("level"));
+		hero.levelUp();
+		assertThat(hero, hasProperty("level", is(2)));
+	}
+
+	@Test
+	public void testHeroTakeDamage() throws Exception {
+		assertThat(hero, hasProperty("hp"));
+		hero.takeDamage(5);
+		assertThat(hero, hasProperty("hp", is(15)));
+	}
+
+	@Test
+	public void testHeroAttack() throws Exception {
+		assertThat(hero, hasProperty("atk"));
+		assertThat(enemy, hasProperty("hp"));
+		hero.attack(enemy);
+
+		assertThat(enemy, hasProperty("hp", lessThanOrEqualTo(13)));
+		assertThat(enemy, hasProperty("hp", greaterThanOrEqualTo(11)));
+
+		assertThat(hero, hasProperty("atk", lessThanOrEqualTo(this.hero.getAtk() +2)));
+		assertThat(hero, hasProperty("atk", greaterThanOrEqualTo(this.hero.getAtk())));
+	}
+
+	@Test
+	public void testHeroProperties() throws Exception {
+		assertThat(hero, hasProperty("name"));
+        assertThat(hero, hasProperty("name", is("Jaina Portvaillant")));
+
+		assertThat(hero, hasProperty("hp"));
+		assertEquals(hero.getHp(), 20);
+
+        assertThat(hero, hasProperty("level"));
+        assertEquals(hero.getLevel(), 1);
+
+		assertThat(hero, hasProperty("atk"));
+		assertSame(2, hero.getAtk());
+	}
+}
